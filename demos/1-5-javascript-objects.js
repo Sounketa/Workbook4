227a1d9163d522f04751d5da0aa9b7385d3524fd
Sounketa <julien.ilboudo97@gmail.com>
2024-05-06 10:22:22 -0400

    object demo

---
+++ demos/1-5-javascript-objects.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// let employee = {
+
+// };
+// console.log(employee);
+
+let employee = {
+    employeeId: "A134L",
+    name: "Ezra", 
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+};
+console.log(employee);
